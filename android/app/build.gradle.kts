1213cce367e97a7cf0f05a4aa057694e5df75a1c
roy601 <apurbor39@gmail.com>
2025-12-09 00:47:33 +0600

    nav

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.bangladesh_landmarks_app"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
